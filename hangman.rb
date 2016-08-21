b87f708be13f035ad529cabc6355527abbf322e1
Rhys <rphillips1@live.com.au>
2016-08-21 15:40:54 +0700

    Completed hangman webapp

--- hangman.rb
+++ hangman.rb
@@ -3,62 +3,46 @@ require 'sinatra/reloader' if development?
 require 'YAML'
 
 word = choose_random_word
-hidden_word = word_attempt(word)
+hint = word_attempt(word)
 rounds_remaining = 12
 guess = nil
 guessed_letters = []
 guessed_letters_string = ""
-hint = ""
+underscore_count = word.length
 
 get '/' do
 	guessed_letters_string = ""
 	guessed_letters =[]
 	rounds_remaining = 12
 	word = choose_random_word
-	hidden_word = word_attempt(word)
-	erb :index, :locals => {:word => word, :hint => hint, :hidden_word => hidden_word, :rounds_remaining => rounds_remaining, :guessed_letters_string => guessed_letters_string}
+	hint = word_attempt(word)
+	underscore_count = word.length
+	erb :index, :locals => {:word => word, :hint => hint, :hint => hint, :rounds_remaining => rounds_remaining, :guessed_letters_string => guessed_letters_string}
 end
 
 post '/' do
 	guess = params['guess'].upcase[0] # captures the guess from text box
 	guessed_letters << guess
 	guessed_letters_string = guessed_letters.join(" ")
-	rounds_remaining -= 1
 	hint = ""
 	if rounds_remaining == 0 #resets game when rounds == 0
 		word = choose_random_word
-		hidden_word = word_attempt(word)
+		hint = word_attempt(word)
 		rounds_remaining = 12
 		guessed_letters_string = ""
 		guessed_letters =[]
 		hint = ""
 	end
-	unless (hint.include("_")) && (hint != "")
-		word = choose_random_word
-		hidden_word = word_attempt(word)
-		rounds_remaining = 12
-		guessed_letters_string = ""
-		guessed_letters =[]
-		hint = ""
-	end
-	word.each_char do |letter|
+	word.each_char do |letter| # iterates through each letter of the word and replaces _ with 'letter' if it exists in the array
 		if guessed_letters.include? letter
 			hint << letter
 		else
 			hint << "_ "
 		end
 	end
-	erb :index, :locals => {:word => word, :hint => hint, :hidden_word => hidden_word, :rounds_remaining => rounds_remaining, :guessed_letters_string => guessed_letters_string}
-end
-
-def new_game
-	puts "-- NEW GAME --"
-	@guessed_letters = [] # array to store all letters that have been guessed
-	@rounds_remaining = 10
-	@word = choose_random_word
-	@underscore_count = @word.length # counts amount of _ in hint. If amount doesn't change (wrong guess) then rounds remaining decreases
-	puts word_attempt(@word)
-	round
+	rounds_remaining -= 1 if underscore_count == hint.scan(/_/).count # deducts round when amount of _'s stay the same
+	underscore_count = hint.scan(/_/).count
+	erb :index, :locals => {:word => word, :hint => hint, :hint => hint, :rounds_remaining => rounds_remaining, :guessed_letters_string => guessed_letters_string}
 end
 
 def word_attempt(word) # returns word as _'s
@@ -76,36 +60,4 @@ def choose_random_word
 		word = File.readlines("dictionary.txt").sample # picks one eligible word from file
 	end
 	word.chomp.upcase
-end
-
-def round
-	if @rounds_remaining == 0 # ends game if rounds run out
-		puts "The word was #{@word.chomp}. You lose."
-		new_game
-	else
-		hint = "" # empty string to store hint
-		puts "Choose a letter:"
-		guess = gets.chomp
-		@validated_guess = guess.upcase[0] # ensures guess is single capital letter
-		@guessed_letters << @validated_guess # adds guess to array of guessed letters
-		@word.each_char do |letter|
-			if @guessed_letters.include? letter
-				hint << "#{letter} "
-			else
-				hint << "_ "
-			end
-		end
-		hint = hint[0..-3]
-		@rounds_remaining -= 1 if @underscore_count == hint.scan(/_/).count
-		@underscore_count = hint.scan(/_/).count
-		puts "Letters used: #{@guessed_letters.join(", ")}"
-		puts hint
-		if hint.include?("_")
-			puts "#{@rounds_remaining} rounds remaining"
-			round
-		else
-			puts "You win!"
-			new_game
-		end
-	end
 end
